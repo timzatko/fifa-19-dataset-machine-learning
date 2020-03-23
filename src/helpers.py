eb1e0abf66c8f1d9f181d235974682b454371683
Timotej Zaťko <timi.zatko@gmail.com>
2020-03-23 12:13:52 +0100

    Implement get_nan_columns()

--- src/helpers.py
+++ src/helpers.py
@@ -1,9 +1,13 @@
-def get_numberic_columns(data_frame):
+def get_numeric_columns(data_frame):
     numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
     return data_frame.select_dtypes(include=numerics)
     
     
-def fill_na(data_frame, value = 0):
+def fill_na(data_frame, value=0):
     for column in data_frame.columns:
         data_frame[column].fillna(value)
     return data_frame
+
+
+def get_nan_columns(df):
+    return df.columns[df.isna().any()].tolist()
