159e25c6f394d299b7d46c3f47487813722489f8
Timotej Zaťko <timi.zatko@gmail.com>
2020-03-30 11:17:29 +0200

    Add regression models with evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,6 +1,9 @@
-from sklearn.metrics import classification_report, plot_confusion_matrix
+from sklearn.metrics import classification_report, plot_confusion_matrix, mean_squared_error, mean_squared_log_error, explained_variance_score
+
 
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 
 def custom_classification_report(clf, labels, x_test, y_test, **kwargs):
@@ -28,3 +31,22 @@ def custom_classification_report(clf, labels, x_test, y_test, **kwargs):
     fig, ax = plt.subplots(figsize=kwargs.get('figsize', (20, 10)))
 
     plot_confusion_matrix(clf, x_test, y_test, display_labels=labels, ax=ax)
+
+    
+def custom_regression_report(clf, x_test, y_test, **kwargs):
+    y_pred = clf.predict(x_test)
+    
+    print(f'MSE: {mean_squared_error(y_test, y_pred)}')
+    print(f'RMSE: {mean_squared_error(y_test, y_pred, squared=False)}')
+#     print(f'MSLE: {mean_squared_log_error(y_test, y_pred)}')
+    print(f'Explained variance - uniform_average (higher is better): {explained_variance_score(y_test, y_pred, multioutput="uniform_average")}')
+    print(f'Explained variance - variance_weighted (higher is better): {explained_variance_score(y_test, y_pred, multioutput="variance_weighted")}')
+    
+    data = pd.DataFrame(data={ 'y_test': y_test / 1000000, 'y_pred': y_pred / 1000000 })
+    max_value = max(max(y_test), max(y_pred)) / 1000000
+    
+    plt.xlim(0, max_value)
+    plt.ylim(0, max_value)
+    sns.scatterplot(x='y_test', y='y_pred', data=data)
+  
+    print('\n')
